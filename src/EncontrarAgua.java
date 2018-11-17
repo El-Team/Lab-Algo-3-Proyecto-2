7e9cd89fc0d239c8e1d12edc6b7a92703d85c200
Luis Paredes <lualps94@gmail.com>
2018-11-17 07:05:23 -0400

    Added attributes to EcontrarAgua

--- src/EncontrarAgua.java
+++ src/EncontrarAgua.java
@@ -3,8 +3,35 @@
  * un número de personas en ese edificio, le asigne el baño a todas el baño más
  * cercano disponible en todos los casos.
  */
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class EncontrarAgua {
+
+	/**
+	 * Grafo inicial tal cual fue importado sin tomar en cuenta las
+	 * modificaciones introducidas por cada caso.
+	 */
+	GrafoNoDirigido<Integer, Integer> baseGraph;
+	/**
+	 * Diccionario donde cada clave corresponde al identificador de un caso y
+	 * cada valor corresponde a un grafo modificado acorde al caso con el
+	 * identificador de la clave.
+	 */
+	LinkedHashMap<String, GrafoNoDirigido<Integer, Integer>> caseBasedGraphs;
+	/**
+	 * Lista de casos importados.
+	 */
+	ArrayList<Case> cases = new ArrayList();
+	/**
+	 * Id del vértice de partida.
+	 */
+	String origin;
+	/**
+	 * Cantidad de personas a las que se les debe asignar baño.
+	 */
+	int numOfPeople;
+
 
 	/**
 	 * Rutina principal
